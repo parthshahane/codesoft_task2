2b1ab028dadeaa13aaf4c5070c6f01d2f80f4e6a
parthshahane <145256446+parthshahane@users.noreply.github.com>
2023-09-24 21:33:56 +0530

    Add files via upload

---
+++ calculator main.py
@@ -0,0 +1,32 @@
+# 1. ADD
+# 2. SUBTRACT
+# 3. MULTIPLY
+# 4. DIVIDE
+
+print("select an operation to perform: ")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+
+operation= input()
+
+if operation == "1" :
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("the sum is " + str(int(num1) + int(num2)))
+elif operation == "2" :
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("the difference is " + str(int(num1) - int(num2)))
+elif operation == "3" :
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("the product is " + str(int(num1) * int(num2)))
+elif operation == "4" :
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("the result is " + str(int(num1) / int(num2)))
+else:
+    print("invalid entry")
+
